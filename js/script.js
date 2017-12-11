12f0253dafd698e8f1b347517a722d51a0c689e7
Mostyn Griffith (NewMAC) <mostyngriffith@mostyns-mbp-3.risd.edu>
2017-12-11 13:57:12 -0500

    first

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+$('.owl-carousel').owlCarousel({
+    items:1,
+    lazyLoad:true,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:2500,
+    loop:true
+});
+});
